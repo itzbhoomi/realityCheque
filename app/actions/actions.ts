b06fac90588f5a5b66bf62a192c61be082aaa67a
itzbhoomi <bhoomisakhrani25@gmail.com>
2025-03-19 19:05:50 +0530

    ##

--- app/actions/actions.ts
+++ app/actions/actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import prisma from "../../lib/prisma";
+import prisma from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 
 // Define the shape of the response for createUser
